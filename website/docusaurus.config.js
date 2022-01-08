e6a35c0b18a18896ad1609de395721c81b4cad7c
Kapil Gupta <kapilguptansit@gmail.com>
2022-01-08 17:03:02 +0530

    modified headers and footers

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -3,107 +3,97 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'System Design Tutorial',
-  tagline: 'Curated guide to help you master system design concepts',
-  url: 'https://systemdesigntutorial.com',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'kapilgupta101292', // Usually your GitHub org/user name.
-  projectName: 'system-design-tutorial', // Usually your repo name.
+(
+	module.exports = {
+		title: 'System Design Tutorial',
+		tagline: 'Curated guide to help you master system design concepts',
+		url: 'https://systemdesigntutorial.com',
+		baseUrl: '/',
+		onBrokenLinks: 'warn',
+		onBrokenMarkdownLinks: 'warn',
+		favicon: 'img/favicon.ico',
+		organizationName: 'kapilgupta101292', // Usually your GitHub org/user name.
+		projectName: 'system-design-tutorial', // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/kapilgupta101292/system-design-tutorial/edit/master/website',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: 'https://github.com/kapilgupta101292/system-design-tutorial/edit/master/website'
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+		presets: [
+			[
+				'@docusaurus/preset-classic',
+				/** @type {import('@docusaurus/preset-classic').Options} */
+				({
+					docs: {
+						routeBasePath: '/',
+						sidebarPath: require.resolve('./sidebars.js'),
+						// Please change this to your repo.
+						editUrl:
+							'https://github.com/kapilgupta101292/system-design-tutorial/edit/master/website',
+					},
+					blog: false,
+					theme: {
+						customCss: require.resolve('./src/css/custom.css'),
+					},
+				}),
+			],
+		],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'System Design Tutorial',
-        logo: {
-          alt: 'System Design Tutorial Logo',
-          src: 'img/logo.png',
-        },
-        items: [
-        {href: '/', label: 'Getting Started', position: 'right'},
-        {to: 'blog', label: 'Blog', position: 'right'},
-        {
-          href: 'https://github.com/kapilgupta101292/system-design-tutorial',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/kapilgupta101292/system-design-tutorial',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Kapil Gupta. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+		themeConfig:
+			/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+			({
+				navbar: {
+					title: 'System Design Tutorial',
+					logo: {
+						alt: 'System Design Tutorial Logo',
+						src: 'img/logo.png',
+					},
+					items: [
+						{ href: '/', label: 'System Design', position: 'right' },
+						{
+							to: 'sde-interview-prep',
+							label: 'SDE Interview Preparation',
+							position: 'right',
+						},
+						{
+							href: 'https://github.com/kapilgupta101292/system-design-tutorial',
+							label: 'GitHub',
+							position: 'right',
+						},
+					],
+				},
+				footer: {
+					style: 'dark',
+					links: [
+						{
+							title: 'Docs',
+							items: [
+								{
+									label: 'System Design',
+									to: '/',
+								},
+								{
+									label: 'SDE Interview Preparation',
+									to: '/sde-interview-prep',
+								},
+							],
+						},
+						{
+							title: 'Community',
+							items: [
+								{
+									label: 'Discord',
+									href: 'https://discord.gg/dWxCK46ZPT',
+								},
+								{
+									label: 'GitHub',
+									href: 'https://github.com/kapilgupta101292/system-design-tutorial',
+								},
+							],
+						},
+					],
+					copyright: `Copyright © ${new Date().getFullYear()} System Design Tutorial. Built with Docusaurus.`,
+				},
+				prism: {
+					theme: lightCodeTheme,
+					darkTheme: darkCodeTheme,
+				},
+			}),
+	}
+);
---
+++ website/src/pages/sde-interview-prep.md
@@ -0,0 +1,24 @@
+---
+title: SDE Interview Preparation
+description: Interview Preparation Resources for Software Development Engineering Roles.
+hide_table_of_contents: false
+---
+
+# Data Structures and Algorithms
+
+## Portals
+
+- [Leetcode](https://leetcode.com/)
+- [TopCoder](https://www.topcoder.com/)
+
+## Resources
+
+- [SDE Sheet by Striver](https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/)
+
+### Binary Search
+
+- [Binary Search Template](https://towardsdatascience.com/powerful-ultimate-binary-search-template-and-many-leetcode-problems-1f850ef95651)
+
+### Backtracking
+
+- [Backtracking Template](<https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)>)
